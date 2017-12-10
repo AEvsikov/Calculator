4bc34135f89abbf19994d1d68645a3bd1c9c1d25
AEvsikov <aevs1986@mail.ru>
2017-12-10 17:58:10 +0300

    добавил операции вычитания, умножения, деления

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,4 +9,20 @@ public class Calculator {
         }
      return result;
     }
+
+    public Double Subtraction(Double a, Double b) {
+        return a - b;
+    }
+
+    public Double Multiplication(List<Double> list){
+        Double result = 0.0;
+        for (Double aDouble : list) {
+            result *= aDouble;
+        }
+        return result;
+    }
+
+    public Double Division(Double a, Double b){
+        return a / b;
+    }
 }
--- src/main/java/CalculatorRunner.java
+++ src/main/java/CalculatorRunner.java
@@ -1,6 +1,5 @@
-import java.nio.DoubleBuffer;
 import java.util.ArrayList;
-        import java.util.List;
+import java.util.List;
 import java.util.Scanner;
 
 public class CalculatorRunner {
@@ -8,21 +7,74 @@ public class CalculatorRunner {
     public static void main(String[] args) {
         // создаем экземпляр класса Calculator
         Calculator myCalculator = new Calculator();
-        System.out.println("Введите числа, которые хотите просуммировать, через знак , ");
+        System.out.println("Если Вы хотите выполнить сложение любого числа чисел, введите: +, если вычитание: -, умножение: *, деление: /");
         // считываем строку из консоли
         Scanner scanner = new Scanner(System.in);
-        String inputstring = scanner.nextLine();
-        // выделяем из строки подстроки, разделенные запятой
-        String[] stringList = inputstring.split(",");
-        List<Double> doubleList = new ArrayList<Double>();
-        // преобразуем строки в Double и складываем их в List, который отправим в метод калькулятора
-        for (String s : stringList) {
-            doubleList.add(Double.parseDouble(s));
+        String operation = scanner.nextLine();
+        //проверяем, что за символ ввел пользователь
+        if(operation.equals("+")){
+            System.out.println("Введите числа, которые хотите просуммировать, через знак , ");
+            // считываем строку из консоли
+            String inputstring = scanner.nextLine();
+            // выделяем из строки подстроки, разделенные запятой
+            String[] stringArray = inputstring.split(",");
+            List<Double> doubleList = new ArrayList<Double>();
+            // преобразуем строки в Double и складываем их в List, который отправим в метод калькулятора
+            for (String s : stringArray) {
+                doubleList.add(Double.parseDouble(s));
+            }
+            // вызываем метод сложения
+            Double result = myCalculator.Summing(doubleList);
+            // выводим результат в консоль
+            System.out.println("Результат сложения: ");
+            System.out.print(String.format("%(.4f", result));
         }
-        // вызываем метод сложения
-        Double a = myCalculator.Summing(doubleList);
-        // выводим результат в консоль
-        System.out.println("Результат сложения: ");
-        System.out.print(String.format("%(.4f", a));
+        if(operation.equals("-")){
+            System.out.println("Введите первое число:");
+            // считываем строку из консоли
+            String a = scanner.nextLine();
+            Double doubleA = Double.parseDouble(a);
+            System.out.println("Введите второе число:");
+            // считываем строку из консоли
+            String b = scanner.nextLine();
+            Double doubleB = Double.parseDouble(b);
+            // вызываем метод вычитания
+            Double result = myCalculator.Subtraction(doubleA, doubleB);
+            // выводим результат в консоль
+            System.out.println("Результат вычитания: ");
+            System.out.print(String.format("%(.4f", result));
+        }
+        if(operation.equals("*")){
+            System.out.println("Введите числа, которые хотите перемножить, через знак , ");
+            // считываем строку из консоли
+            String inputstring = scanner.nextLine();
+            // выделяем из строки подстроки, разделенные запятой
+            String[] stringArray = inputstring.split(",");
+            List<Double> doubleList = new ArrayList<Double>();
+            // преобразуем строки в Double и складываем их в List, который отправим в метод калькулятора
+            for (String s : stringArray) {
+                doubleList.add(Double.parseDouble(s));
+            }
+            // вызываем метод умножения
+            Double result = myCalculator.Multiplication(doubleList);
+            // выводим результат в консоль
+            System.out.println("Результат сложения: ");
+            System.out.print(String.format("%(.4f", result));
+        }
+        if(operation.equals("/")){
+            System.out.println("Введите первое число:");
+            // считываем строку из консоли
+            String a = scanner.nextLine();
+            Double doubleA = Double.parseDouble(a);
+            System.out.println("Введите второе число:");
+            // считываем строку из консоли
+            String b = scanner.nextLine();
+            Double doubleB = Double.parseDouble(b);
+            // вызываем метод деления
+            Double result = myCalculator.Division(doubleA, doubleB);
+            // выводим результат в консоль
+            System.out.println("Результат деления: ");
+            System.out.print(String.format("%(.4f", result));
+        }
     }
 }
